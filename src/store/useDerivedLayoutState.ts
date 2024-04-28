ccaf56a2fedb704d27eb02055a647d096dcacb39
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-04-29 03:05:10 +0530

    dlt

--- src/store/useDerivedLayoutState.ts
+++
@@ -1,6 +0,0 @@
-import { SHORT_FRAME_HEIGHT, useLayoutStore } from '@/store/layout';
-
-export function useDerivedLayoutState() {
-  const frameSize = useLayoutStore((store) => store.frameSize);
-  return { isShortFrame: frameSize.height === SHORT_FRAME_HEIGHT };
-}
