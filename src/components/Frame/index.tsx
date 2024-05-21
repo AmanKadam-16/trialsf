a381cd7f75f7fedb188bb1777c00dbb91e894d15
PR0T0-TYPE <168283814+PR0T0-TYPE@users.noreply.github.com>
2024-05-21 16:12:09 +0000

    ai-interviewer-border

--- src/components/Frame/index.tsx
+++ src/components/Frame/index.tsx
@@ -7,11 +7,10 @@ export type FrameProps = PropsWithChildren;
 const frameStyles = cva([
   'rounded-[25px]',
   'backdrop-blur-[10px]',
-  'absolute bottom-0 right-0',
   'text-black',
   'antialiased',
   'transition-colors',
-  'outline outline-2 -outline-offset-2 outline-white/80',
+ 
   'bg-tan-200/70', // Apply the open state background color by default
 ]);
 
@@ -33,18 +32,21 @@ export const Frame: FC<FrameProps> = ({ children }) => {
   }, []);
 
   const frameDimensions = {
-    width: `${windowDimensions.width}px`,
-    height: `${windowDimensions.height}px`,
+    width: `${windowDimensions.width * 0.7}px`, // 80% of the window width
+    height: `${windowDimensions.height * 0.7}px`, // 80% of the window height
   };
 
   return (
-    <motion.div className={`${frameStyles()} w-full h-full`}
-     
-      animate={frameDimensions}
-      transition={{ duration: 0.2, ease: [0.2, 0, 0, 1] }}
-      data-testid={'frame-component'}
-    >
-      {children}
-    </motion.div>
+    <div className="flex items-center justify-center ">
+      <motion.div
+        className={`${frameStyles()} relative`}
+        style={{ width: frameDimensions.width, height: frameDimensions.height }}
+        animate={frameDimensions}
+        transition={{ duration: 0.2, ease: [0.2, 0, 0, 1] }}
+        data-testid={'frame-component'}
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 };
