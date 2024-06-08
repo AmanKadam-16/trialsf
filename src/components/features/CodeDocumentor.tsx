7066311e01a131ae6bca01dc28eaef3b7002e60b
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-06-08 06:16:03 +0000

    token-extension

--- src/components/features/CodeDocumentor.tsx
+++ src/components/features/CodeDocumentor.tsx
@@ -128,8 +128,8 @@ const CodeDocumentor = () => {
           { role: 'system', content:'<|start_header_id|>system<|end_header_id|>You are an AI Code Documentor so perform documentation of provided code according to Github Markdown Rules.Don\'t entertain any non-coding inquiries.If your response contains code blocks then provide the generated code within Markdown code blocks, specifying the language (e.g., ```javascript or ```python).<|eot_id|> ' },
           { role: 'user', content: `<|start_header_id|>user<|end_header_id|>${currentInput}<|eot_id|> `},
         ],
-        model: 'meta-llama/Llama-3-70b-chat-hf',
-        max_tokens: 7000,
+        model: 'mistralai/Mixtral-8x22B-Instruct-v0.1',
+        max_tokens: 30000,
         stream: true,
       });
   
